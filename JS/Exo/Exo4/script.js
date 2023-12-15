863a6397ae997ed7c756227bf00a4c6906ec414d
Nicolas Queva <nicolas.queva62@gmail.com>
2023-12-15 12:14:44 +0100

    modif exo 4

--- JS/Exo/Exo4/script.js
+++ JS/Exo/Exo4/script.js
@@ -6,9 +6,9 @@ mdp = prompt("Veuillez rentre un mdp : ")
 affichage = `<p>Le mot de passe entrer est ${mdp}</p>`
 affichage += `<p>Le mot de passe inversé est ${ mdpreverse= mdp.split('').reverse().join('')}</p>`
 if (mdp == mdpreverse){
-    affichage += `le ${mdp} est un palimdrome`
+    affichage += `Le ${mdp} est un palimdrome`
 }
 else{
-    affichage += `le ${mdp} n'est pas un palimdrome`
+    affichage += `Le ${mdp} n'est pas un palimdrome`
 }
 affich.innerHTML = affichage
