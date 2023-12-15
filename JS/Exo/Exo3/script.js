f752cb01c267b91db0d755111cf1b3acaf3f15e6
Nicolas Queva <nicolas.queva62@gmail.com>
2023-12-15 14:42:04 +0100

    maj exo

--- JS/Exo/Exo3/script.js
+++ JS/Exo/Exo3/script.js
@@ -3,13 +3,15 @@ let  pi, nbr1, aire, permimetre,affichage
 
 pi = Math.PI
 nbr1 = Number(prompt("Veuillez saisir le diamètre du cercle :" ))
-affichage = `<p>Utilisation de la constante &#960</p>`
-affichage += `<p>La valeur de &#960 est de : ${pi}`
-affichage += `<p> Calcul du périmètre (d x &#960) et de l'aire (&#960 * r²) d'un cercle </p>`
-affichage += `<p>La diamètre est de : ${nbr1}cm`
-affichage += `<p>La permimetre est de : ${ permimetre = nbr1 * Math.PI}cm`
-affichage += `<p>L'aire  est de :${aire = ((nbr1/2)*(nbr1/2)) * Math.PI}cm²`
-affichage += `<p>La permimetre est de : ${Math.round(permimetre)}cm (arrondi)`
-affichage += `<p>L'aire'est de : ${Math.round(aire)}cm² (arrondi)`
+affichage = `
+<h3>Utilisation de la constante &#960 (pi)</h3>
+<p>La valeur de &#960 est de : ${pi}
+<h3> Calcul du périmètre (d x &#960) et de l'aire (&#960 * r²) d'un cercle </h3>
+<p>La diamètre est de : ${nbr1}cm
+<p>La permimetre est de : ${ permimetre = nbr1 * Math.PI}cm
+<p>L'aire  est de :${aire = Math.pow((nbr1/2),2)* Math.PI}cm²
+<p>La permimetre est de : ${Math.round(permimetre)}cm (arrondi)
+<p>L'aire est de : ${Math.round(aire)}cm² (arrondi)
+`
 
 affich.innerHTML = affichage
--- JS/Exo/Exo4/script.js
+++ JS/Exo/Exo4/script.js
@@ -1,10 +1,12 @@
 const affich = document.querySelector(".affichage")
 let mdp, mdpreverse,affichage
 
-mdp = prompt("Veuillez rentre un mdp : ")
+mdp = prompt("Veuillez rentre un mot de passe : ")
 
-affichage = `<p>Le mot de passe entrer est ${mdp}</p>`
-affichage += `<p>Le mot de passe inversé est ${ mdpreverse= mdp.split('').reverse().join('')}</p>`
+affichage = `
+<p>Le mot de passe entrer est ${mdp}</p>
+<p>Le mot de passe inversé est ${ mdpreverse= mdp.split('').reverse().join('')}</p>
+`
 if (mdp == mdpreverse){
     affichage += `Le ${mdp} est un palimdrome`
 }
