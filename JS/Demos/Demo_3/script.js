35920eff155842fffb852533ebbdb3586901c799
Nicolas Queva <nicolas.queva62@gmail.com>
2023-12-15 10:59:23 +0100

    Demo Js

---
+++ JS/Demos/Demo_3/script.js
@@ -0,0 +1,39 @@
+let nb1, nb2
+
+// L'addition : 
+nb1 = 15
+nb2 = 12
+console.log(nb1 + nb2);
+
+// Soustraction : 
+console.log(nb1 - nb2)
+
+//Multiplication :
+console.log(nb1 * nb2)
+
+// Division :
+console.log(nb1 / nb2)
+
+// Modulo : 
+console.log(nb1 % nb2);
+
+// Incrementation et Décrementation
+console.log(nb1 = nb1 + 1);
+console.log(nb1 += 1); // operateur combné +=
+nb1++
+console.log(nb1);
+
+console.log(nb1 = nb1 - 1);
+console.log(nb1 -= 1); // operateur combné +=
+nb1--
+console.log(nb1);
+
+console.log("--------");
+// Affichage de la valeur puis incrémentation
+console.log(nb1++);
+//Incrémentation puis affichage de la valeur
+console.log(++nb1);
+//equivalent a nb1 = nb1 * nb2 
+console.log(nb1 *= nb2); //Multiplication combiné
+//Idem mais /
+console.log(nb1/=nb2); //Division combiné
