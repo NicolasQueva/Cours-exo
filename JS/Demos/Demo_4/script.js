98e1b093eb4d7c40157eddd74956f5fae8f228cf
Nicolas Queva <nicolas.queva62@gmail.com>
2023-12-19 14:40:50 +0100

    exo et cours

--- JS/Demos/Demo_4/script.js
+++ JS/Demos/Demo_4/script.js
@@ -49,3 +49,47 @@ if (!estVrai2) {
 else {
     console.log("Vrai");
 }
+
+
+/*
+    Switch Case
+
+    switch (conditon){
+        case valeur1:
+            instruction
+            break;
+        case valeur2:
+            instruction
+            break;
+        default:
+            instruction
+            break; 
+    }
+*/
+
+let fruit = "banane"
+
+    switch (fruit) {
+        case "Orange":
+            console.log("Les oranges sont a 2 €");
+            break;
+        case "banane":
+            console.log("Les bananes sont a 1 euros");
+            break;
+        default:
+            console.log("Votre fruit n'est pas disponible");
+            break;
+    }
+
+// Switch range de valeurs
+
+age = 7
+
+switch (age) {
+    case age < 3:
+        console.log("Votre enfant est trop jeune");
+        break;
+    case age <= 6: 
+        console.log("Votre enfant est dans la catégorie Baby");
+        break;
+}
