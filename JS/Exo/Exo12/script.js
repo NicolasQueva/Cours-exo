0c57fe68ab3f09f205e1a7b74b60afbdc3029288
Nicolas Queva <nicolas.queva62@gmail.com>
2023-12-19 09:02:53 +0100

    Modif exo 12-13

--- JS/Exo/Exo12/script.js
+++ JS/Exo/Exo12/script.js
@@ -4,11 +4,13 @@ let affich, resultat, nb1, nb2
 
 nb1 = Number(prompt("Veuillez entrer un premier nombre : "))
 nb2 = Number(prompt("Veuillez entrer un second nombre :"))
-resultat = nb1%nb2
-if ( resultat == 0) {
+resultat = nb1 % nb2
+if (resultat == 0) {
     affich = `${nb1} est divisible par ${nb2}, le resultat est ${resultat}`
 } else {
     affich = `${nb1} n'est pas divisible par ${nb2}, le resultat est ${resultat}`
 }
 
+// Ternaire 
+// nb % nb == 0 ? affich = `${nb1} est divisible par ${nb2}, le resultat est ${resultat}` : affich = `${nb1} n'est pas divisible par ${nb2}, le resultat est ${resultat}` 
 affichage.innerHTML = affich
--- JS/Exo/Exo13/script.js
+++ JS/Exo/Exo13/script.js
@@ -3,20 +3,23 @@ const affichage = document.querySelector(".affichage")
 let affich, age
 
 age = Number(prompt("Veuillez entrer l'âge de votre enfant "))
-
-if (age < 3) {
-    affich = `L'enfant est trop jeune pour pratiquer `
-} else if (age <= 3 || age <= 6) {
-    affich = `L'enfant est dans la catégorie Baby `
-} else if (age <= 7 || age <= 8) {
-    affich = `L'enfant est dans la catégorie Poussin `
-} else if (age <= 9 || age <= 10) {
-    affich = `L'enfant est dans la catégorie Pupille `
-} else if (age <= 11 || age <= 12) {
-    affich = `L'enfant est dans la catégorie Minime `
-} else if (age <= 13 || age < 18) {
-    affich = `L'enfant est dans la catégorie cadet `
+if (age > 0) {
+    if (age < 3) {
+        affich = `L'enfant est trop jeune pour pratiquer `
+    } else if (age <= 3 || age <= 6) {
+        affich = `L'enfant est dans la catégorie Baby `
+    } else if (age <= 7 || age <= 8) {
+        affich = `L'enfant est dans la catégorie Poussin `
+    } else if (age <= 9 || age <= 10) {
+        affich = `L'enfant est dans la catégorie Pupille `
+    } else if (age <= 11 || age <= 12) {
+        affich = `L'enfant est dans la catégorie Minime `
+    } else if (age <= 13 || age < 18) {
+        affich = `L'enfant est dans la catégorie cadet `
+    } else {
+        affich = `C'est n'est plus un enfant`
+    }
 } else {
-    affich = `C'est n'est plus un enfant`
+    alert("Entrez un âge valide !s")
 }
 affichage.innerHTML = affich
